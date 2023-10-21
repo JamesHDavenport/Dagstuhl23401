606a71fc0a99039cd876939fda3a16678658b489
Edgar Costa <edgarc@mit.edu>
2023-10-20 22:25:31 -0400

    finsum_succ

--- verify-irred/VerifyIrred/MahlerMeasure2.lean
+++ verify-irred/VerifyIrred/MahlerMeasure2.lean
@@ -170,8 +170,8 @@ lemma L2normSq_smul_eq (g : ℂ[X]) (α: ℂ) : L2normSq ( α • g ) = ‖α‖
 lemma L2normSq_X_mul_eq (g : R[X]) :  L2normSq ( X * g ) =  L2normSq ( g ) := by
   simp [L2normSq]
 
-lemma foobar_1  (g : ℂ[X]) (α β: ℂ) :
-    L2normSq ( ( α • X - Polynomial.C β ) * g ) =  ∑ᶠ (a : ℕ), if a = 0 then ‖β * g.coeff 0‖^2 else ‖α * g.coeff (a - 1) - β * g.coeff a‖^2  := by
+lemma foobar_2  (g : ℂ[X]) (α β: ℂ) :
+    L2normSq ( ( α • X - Polynomial.C β ) * g ) =  ‖β * g.coeff 0‖^2 + ∑ᶠ (a : ℕ) (ha : a ≠ 0), ‖α * g.coeff (a - 1) - β * g.coeff a‖^2  := by
   -- g = 1 LHS 1^2 + |alpha|^2
   -- RHS |alpha^2|
   simp [L2normSq_finsum]
@@ -183,17 +183,90 @@ lemma foobar_1  (g : ℂ[X]) (α β: ℂ) :
   --rw [coeff_smul_pow_add_mul]
   -/
 
+lemma foobar_1  (g : ℂ[X]) (α β: ℂ) :
+    L2normSq ( ( α • X - Polynomial.C β ) * g ) =  ∑ᶠ (a : ℕ) , ‖α * (if a ≠ 0 then g.coeff (a - 1) else 0) - β * g.coeff a‖^2  := by
+  -- g = 1 LHS 1^2 + |alpha|^2
+  -- RHS |alpha^2|
+  simp [L2normSq_finsum]
+  sorry
+  /-
+  rw [Finsum.sum_eq_sum_diff_singleton_add (i := 0)]
+  simp [coeff_smul_pow_neg_mul'']
+  --simp [L2normSq, L2normSq_smul_eq]
+  --rw [coeff_smul_pow_add_mul]
+  -/
+lemma finsum_if_mul (f : ℕ → R) (α : R) (hf : Set.Finite f.support ) :
+  ∑ᶠ (i : ℕ), (if i = 0 then 0 else f i * α) = α * (∑ᶠ (i : ℕ), (if i = 0 then 0 else f i)) := by
+  rw [mul_comm]
+  rw [finsum_mul]
+  simp only [ite_mul, zero_mul]
+  apply Set.Finite.subset hf
+  simp
+
+lemma finsum_succ (f : ℕ → R) (hf : Set.Finite f.support ) (hg : f 0 = 0) :
+  ∑ᶠ (i : ℕ), f i = ∑ᶠ (i : ℕ), f (i + 1) := by
+  simp_rw [← Nat.succ_eq_add_one]
+  rw [← @finsum_mem_univ]
+  rw [← Nat.zero_union_range_succ]
+  rw [finsum_mem_union']
+  simp only [Set.mem_singleton_iff, finsum_cond_eq_left, Set.mem_setOf_eq]
+  rw [hg]
+  simp only [zero_add]
+  rw [finsum_mem_range]
+  apply Nat.succ_injective
+  simp only [Nat.range_succ, Set.disjoint_singleton_left]
+  apply Set.Finite.subset hf
+  simp only [Set.inter_subset_right]
+  apply Set.Finite.inter_of_right hf
+
+
+   
+
+
+
+
+
+  -- rw [← Finset.insert_eq]
+  -- rw [← finsum_mem_insert_zero]
+
+  -- rw [finsum_eq_finset_sum_of_support_subset, finsum_eq_finset_sum_of_support_subset]
+  -- rw [finsum_eq_of_bijective]
+  -- rw [Finset.sum_eq_add_sum_diff_singleton 0]
+
+#exit
+lemma finsum_shift (g : R[X]) (f : R -> R) :
+  ∑ᶠ (i : ℕ), (if i = 0 then 0 else f (g.coeff i - 1)) = ∑ᶠ (i : ℕ), f (g.coeff i) := by
+
+
+
+  -- rw [finsum_eq_of_bijective]
+#exit
+
 open Complex
 lemma foobar (g : ℂ[X]) (α: ℂ) :
-    L2normSq ( (X - Polynomial.C α ) *g ) = L2normSq (( (conj α) • X - ↑ 1) * g) := by
+    L2normSq ( (X - Polynomial.C α ) *g ) = L2normSq (( (conj α) • X - Polynomial.C 1) * g) := by
   have := foobar_1 (β := α) (α := 1)
-  simp at this
+  simp only [one_smul, ne_eq, ge_iff_le, ite_not, mul_ite, mul_zero, one_mul, norm_eq_abs] at this 
   rw[this]
+  rw [map_one]
   have := foobar_1 (β := 1) (α := conj α)
-  simp at this
+  simp only [map_one, ne_eq, ge_iff_le, ite_not, mul_ite, mul_zero, one_mul, norm_eq_abs] at this 
   rw[this]
   simp_rw [Complex.sq_abs]
   simp_rw [Complex.normSq_sub]
+  simp_rw [map_mul]
+  simp_rw [← mul_assoc]
+  simp_rw [mul_comm _ (coeff _ _)]
+  rw [finsum_sub_distrib, finsum_sub_distrib]
+  simp only [ge_iff_le, ite_mul, zero_mul, sub_left_inj]
+  rw [finsum_add_distrib, finsum_add_distrib]
+  simp_rw [apply_ite]
+  simp only [_root_.map_zero, ge_iff_le, map_mul, normSq_conj]
+  sorry
+
+
+  -- rw?
+  -- [@mul_assoc]
 
 
 
